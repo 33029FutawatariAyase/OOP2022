f1edc5ea511d6aed486113a3f589d0e7f46e49c1
33029二渡絢世 <33029@ojs.ac.jp>
2022-04-22 15:37:24 +0900

    FeetConverterクラスの作成

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -20,28 +20,22 @@ namespace DistanceConverter {
 
         //メートルからフィートへの対応表を出力
         private static void PrintMeterToFeetList(int start, int stop) {
+            FeetConverter fc = new FeetConverter();
             for (int meter = 1; meter <= 10; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet = fc.MeterToFeet(meter);
                 Console.WriteLine("{0}m = {1:0.0000}ft", meter, feet);
             }
         }
 
         //フィートからメートルへの対応表を出力
         private static void PrintFeetToMeterList(int start,int stop) {
-            for (int feet = 1; feet <= 10; feet++) {
-                double meter = FeetToMeter(feet);
+            FeetConverter fc = new FeetConverter();
+            for (int feet = 1; feet <= 10; feet++) { 
+                double meter = fc.FeetToMeter(feet);
                 Console.WriteLine("{0}ft = {1:0.0000}m", feet, meter);
             }
         }
 
-        //フィートからメートルを求める
-        static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-
-        //メートルからフィートを求める
-        static double MeterToFeet(int meter) {
-            return meter / 0.3048;
-        }
+        
     }
 }
