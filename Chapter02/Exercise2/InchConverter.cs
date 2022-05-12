e037362f84eec5022548466085baa18933357796
33029二渡絢世 <33029@ojs.ac.jp>
2022-05-12 14:24:24 +0900

    問題2.2完成

---
+++ Chapter02/Exercise2/InchConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class InchConverter {
+
+        private const double ratio = 0.0254;
+
+        //メートルからインチを求める(静的メソッド)
+        public static double MeterToInch(double meter) {
+            return meter / ratio;
+        }
+
+        //インチからメートルを求める(静的メソッド)
+        public static double InchToMeter(double inch) {
+            return inch * ratio;
+        }
+    }
+}
