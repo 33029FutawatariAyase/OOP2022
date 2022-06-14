091ddea36a98f15d73f282bdfceff075f7d2a53a
33029二渡絢世 <33029@ojs.ac.jp>
2022-06-14 15:31:53 +0900

    Test01,Test02完成

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -10,30 +10,38 @@ namespace Test01 {
 
             _score = ReadScore(filePath);
 
-
         }
 
-
-        //メソッドの概要： 
+        //メソッドの概要： 学生のデータを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
 
+            List<Student> students = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(student);
+            }
 
-
-
-
-
-            
+            return students;           
         }
 
-        //メソッドの概要： 
+        //メソッドの概要： 科目別の点数を集計する
         public IDictionary<string, int> GetPerStudentScore() {
 
+            var dict = new Dictionary<string, int>();
+            foreach (var student in _score) {
+                if (dict.ContainsKey(student.Subject))
+                    dict[student.Subject] += student.Score;
+                else
+                    dict[student.Subject] = student.Score;
+            }
 
-
-
-
-            
+            return dict;
         }
     }
 }
