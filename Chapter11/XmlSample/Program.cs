e1ce352b6083f673b111fcf303067bc3075b7345
33029二渡絢世 <33029@ojs.ac.jp>
2022-06-28 15:57:54 +0900

    途中経過

---
+++ Chapter11/XmlSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace XmlSample {
+    class Program {
+        static void Main(string[] args) {
+            using (var wc = new WebClient()) {
+                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/media/anmanmv/all.xml");
+
+                var xdoc = XDocument.Load(stream);
+                var xtitle = xdoc.Root.Descendants("item").Select(x => (string)x.Element("title"));
+
+                foreach (var data in xtitle) {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
