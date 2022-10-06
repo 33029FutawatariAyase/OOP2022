75207768ddecd9d61efb61e8394a05189d7fd225
33029二渡絢世 <33029@ojs.ac.jp>
2022-10-06 15:58:27 +0900

    第15章演習問題　15.1.5まで

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -26,18 +26,49 @@ namespace Exercise1 {
 
         private static void Exercise1_2() {
             
+            var max = Library.Books.Max(b => b.Price);
+            var book = Library.Books.First(b => b.Price == max);
+            Console.WriteLine(book);
+
         }
 
         private static void Exercise1_3() {
-            
+
+            var query = Library.Books.GroupBy(b => b.PublishedYear);
+            foreach (var item in query.OrderBy(b => b.Key)) {
+                Console.WriteLine("{0}年 {1}冊",item.Key,item.Count());
+            }
         }
 
         private static void Exercise1_4() {
-            
+            var query = Library.Books
+                .Join(Library.Categories, //結合する2番目のシーケンス
+                      book => book.CategoryId, //対象シーケンスの結合キー
+                      category => category.Id, //２番目のシーケンスの結合キー
+                      (book, Category) => new {
+                          book.Title,
+                          book.PublishedYear,
+                          book.Price,
+                          CategoryName = Category.Name
+                      }).OrderByDescending(x => x.PublishedYear)
+                        .OrderByDescending(x => x.Price);
+            foreach (var item in query) {
+
+                Console.WriteLine("{0}年 {1}円 {2} ({3})",item.PublishedYear,item.Price,item.Title,item.CategoryName);
+            }
+
         }
 
         private static void Exercise1_5() {
-            
+            var names = Library.Books.Where(b => b.PublishedYear == 2016)
+                .Join(Library.Categories,
+                    book => book.CategoryId,
+                    Category => Category.Id,
+                    (book, category) => category.Name)
+                    .Distinct();
+            foreach (var name in names) {
+                Console.WriteLine(name);
+            }
         }
 
         private static void Exercise1_6() {
