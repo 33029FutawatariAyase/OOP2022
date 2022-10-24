07bdb69d3412e390e22d5b25daa29b29e64685f4
33029二渡絢世 <33029@ojs.ac.jp>
2022-10-24 15:03:20 +0900

    CollorChecker途中

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,6 +20,35 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            seasonComboBox.SelectedIndex = getSeasonIndex();
+        }
+
+        private int getSeasonIndex() {
+            var dt = DateTime.Now;
+            int ret = 0;
+            switch (dt.Month) {
+                case 3:
+                case 4:
+                case 5:
+                    ret = 0;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    ret = 1;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    ret = 2;
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    ret = 3;
+                    break;
+            }
+            return ret;
         }
 
         private void checkBox_Checked(object sender, RoutedEventArgs e) {
